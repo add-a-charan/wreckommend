75a908d9cecd6a4642df5cb7d8246b68db50620b
Sricharan Addagatla <daboss.charan1@gmail.com>
2026-08-12 18:55:27 -0400

    Working database

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from wreckommend import config
 from wreckommend.navidrome.client import SubsonicClient
-import json
+from wreckommend.navidrome.ingest import retrieve_all_albums, ingest
+from wreckommend.storage.db import get_connection
 
 client = SubsonicClient(
     config.URL, config.USER, config.PASSWORD, config.VERSION, config.CLIENT_NAME
@@ -8,8 +9,19 @@ client = SubsonicClient(
 
 
 def main():
-    albums = client.getAlbumList2("random")["albumList2"].get("album", [])
-    print(json.dumps(client.getAlbum(albums[0]["id"]), indent=2))
+    albums = retrieve_all_albums(client)
+    print(f"Found {len(albums)} albums")
+
+    conn = get_connection()
+    try:
+        for i, album in enumerate(albums, start=1):
+            tracks = client.getAlbum(album["id"])["album"]["song"]
+            ingest(conn, tracks)
+            print(
+                f"[{i}/{len(albums)}] Ingested '{album.get('name')}' ({len(tracks)} tracks)"
+            )
+    finally:
+        conn.close()
 
 
 if __name__ == "__main__":
